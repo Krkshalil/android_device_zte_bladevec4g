c3d6a56ae98172ca4b39aa50c44716d0eb228c2b
Halil <krkshalil@outlook.com>
2017-04-26 23:19:38 +0300

    Fix build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,7 +64,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=qcom user_debug=31 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 TARGET_KERNEL_CONFIG := msm8226_P892T50_defconfig
 
 # Keymaster
